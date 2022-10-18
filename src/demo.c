d57d20791eeabd630e20b0ebbe6f3c670acbc335
scefrom <pietrosenesi123@gmail.com>
2022-10-18 22:08:55 +0200

    updated .gitignore

--- src/demo.c
+++ src/demo.c
@@ -10,6 +10,6 @@ int main(int argc, char **argv) {
 	if (argc > 1) {
 		bfr_t *file_buffer = file_read_tfile(argv[1]);
 
-		printf("file_buffer:\n\n%s\n\n", (char*) file_buffer->bfr);
+		printf("file_buffer:\n\n%s\n", (char*) file_buffer->bfr);
 	}
 }
